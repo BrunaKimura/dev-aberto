e248ab8854207a43ca40da8569f2853bb451c455
brunakimura <brunamk@al.insper.edu.br>
2020-03-30 18:40:24 -0300

    Tradução básica

--- aulas/05-localizacao/cli.py
+++ aulas/05-localizacao/cli.py
@@ -1,13 +1,20 @@
 import babel
+from babel.dates import format_datetime
+from babel.numbers import format_number
 from datetime import date
 from datetime import date, datetime, time
+import gettext
+
+gettext.install('cli', localedir='locale')
 
 if __name__ == '__main__':
     today = date.today()
+    today = format_datetime(today, locale='pt_BR')
     print(today)
 
     number = 240000000000.32212
+    number = format_number(number)
     print(number)
     
-    name = input('Input your name: ')
-    print('Hello {}'.format(name))
+    name = input(_('Input your name: '))
+    print(_('Hello {}').format(name))
